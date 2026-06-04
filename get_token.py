697f113bce0d76e4584630ca41314cef90f98ecb
fran10040407-pixel <fran10040407@gmail.com>
2026-06-04 13:02:11 +0900

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -3,11 +3,11 @@ import requests
 url = "https://kauth.kakao.com/oauth/token"
 data = {
     "grant_type": "authorization_code",
-    "client_id": "9fe627ca757d13776ce0c25d19abd2fd",
+    "client_id": "3791bbf5d2e9b1d9b12cece3da1cf583",
     "redirect_uri": "https://localhost",
-    "code": "jzO9zc-JjlBXWDSWBcIohGZ6Era8ahRsyKDZmzy0dKt6BG1QJg6u-QAAAAQKFzVXAAABnpA5A3xPBWDH3LuH7A"
+    "code": "u9_dRNTsufD7KUPluaGxds3SVW-BGdZj_BMnnZkLF5k31WIZwz9DRgAAAAQKFxafAAABnpDKseTNsk3jZ7dWzg"
 }
 
 response = requests.post(url, data=data)
-print("jzO9zc-JjlBXWDSWBcIohGZ6Era8ahRsyKDZmzy0dKt6BG1QJg6u-QAAAAQKFzVXAAABnpA5A3xPBWDH3LuH7A")
+print("=== 발급된 카카오 토큰 정보 ===")
 print(response.text)
